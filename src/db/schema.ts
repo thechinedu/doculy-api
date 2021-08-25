0f0846323d0172d86e7720b979b05a5825c32120
Chinedu Daniel <chinedudaniel7@gmail.com>
2021-08-25 10:52:33 +0200

    Update deps

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,13 +1,11 @@
-import { generateSchema } from "prisma-prime";
+import { generateSchema, env } from "prisma-prime";
 
 import * as models from "./models/index";
-
-const { DATABASE_URL } = process.env;
 
 export const schema = generateSchema({
   datasource: {
     provider: "postgresql",
-    url: DATABASE_URL as string,
+    url: env("DATABASE_URL"),
   },
   models,
 });
